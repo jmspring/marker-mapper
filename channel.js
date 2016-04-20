49a2f6c04fd1ec4d4825bac22f2e31b217d81660
Jim Spring <jmspring@gmail.com>
2016-04-20 16:43:57 -0700

    update socketio flow

--- channel.js
+++ channel.js
@@ -3,6 +3,8 @@ var socketioclient = require('socket.io-client');
 
 var config = require('./config/config');
 
+
+
 var channel = {
     io: null,
     initialize: function(server) {
@@ -11,8 +13,27 @@ var channel = {
         var client = io
             .of('/client')
             .on('connection', function(socket) {
-                var clientpipe;
-                var pipeconnected = false;
+                // state information
+                var fetchpipe = null;
+                var fetchpipeconnected = false;
+                var clientinfo = null;
+                var statusTimeout = null;
+                
+                // browser-mapper socket
+                // socket events
+                socket.on('error', function(err) {
+                    console.log('Error: ' + err);
+                });
+                socket.on('disconnect', function() {
+                    console.log('Disconnecting from browser client.');
+                    if(fetchpipe) {
+                        fetchpipeconnected = false;
+                        fetchpipe.disconnect();
+                        fetchpipe = null;
+                    }
+                });
+
+                // browser driven events
                 socket.on('hello', function(data) {
                     // data is a json blob having the following structure
                     //  {
@@ -20,40 +41,46 @@ var channel = {
                     //      "lat": <latitude>,
                     //      "lon": <longitude,
                     //  }
-                    console.log("Client information: " + data);
+                    console.log('Client information: ' + data);
+                    clientinfo = JSON.parse(data);
                     
-                    // now open pipe to data service
-                    clientpipe = socketioclient.connect('http://' + config.fetchsvc + ':' + config.fetchport + '/points');
-                    clientpipe.on('connect', function() {
-                        console.log("Connected to client pipe.");
-                        pipeconnected = true;
-                        clientpipe.emit('client', data);
+                    var pipeurl = 'http://' + config.fetchsvc + ':' + config.fetchport + '/points';
+                    console.log('Attempting to connect to: ' + pipeurl);
+                    fetchpipe = socketioclient.connect(pipeurl, {
+                        'forceNew': true,
+                        'reconnection': true,
+                        'reconnectionDelay': 1000,
+                        'reconnectionMaxDelay': 6000,
+                        'reconnectionAttempts': 6
+                    });
+                    fetchpipe.on('connect', function() {
+                        console.log('Connected to ' + pipeurl);
+                        fetchpipeconnected = true;
+                        fetchpipe.emit('client', data);
                         socket.emit('status');
                     });
-                    clientpipe.on('point', function(data) {
-                        console.log("Sending client point: " + data);
+                    fetchpipe.on('disconnect', function() {
+                        console.log('Client pipe disconnected.');
+                        fetchpipeconnected = false;
+                    });
+                    fetchpipe.on('reconnect', function() {
+                        fetchpipe.emit('client', data);
+                        socket.emit(status);
+                    });
+                    fetchpipe.on('point', function(data) {
+                        console.log('Sending client point: ' + data);
                         socket.emit('point', data);
                     });
-                    clientpipe.on('disconnect', function() {
-                        console.log("Client pipe disconnected.");
-                        pipeconnected = false;
+                    fetchpipe.on('done', function() {
+                        console.log('Telling client done.');
+                        socket.emit('done');
+                        fetchpipe.disconnect();
                     });
                 });
                 socket.on('status', function(data) {
-                    console.log("Sending status to client pipe: " + data);
-                    if(pipeconnected) {
-                        clientpipe.emit('status', data)
-                    } else {
-                        setTimeout(function() {
-                            socket.emit('status')
-                        }, 500);
-                    }
-                });
-                socket.on('disconnect', function() {
-                    if(clientpipe) {
-                        clientpipe.disconnect();
-                        clientpipe = null;
-                        pipeconnected = false;
+                    console.log('Sending browser status to fetch pipe: ' + data);
+                    if(fetchpipeconnected) {
+                        fetchpipe.emit('status', data)
                     }
                 });
             });
